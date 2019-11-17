14df1c533a4430ad7974af4e0c05d3666d156f5e
Pierre Gauvreau <49109135+PrimeIdeal@users.noreply.github.com>
2019-11-17 15:39:38 -0700

    sketch of integration function

--- integrals.py
+++ integrals.py
@@ -54,10 +54,60 @@ def gen_hypercube(x_range, p_range, N):
         p_idx = r.randint(0, len(p_samples) - 1)
         yield np.array([x_samples.pop(x_idx), p_samples.pop(p_idx)])
 
+def winding_integrand(E_i, E):
+    """
+    The transformed winding number function to be integrated over
+    the domain enclosed by the trajectory.
+    Parameters
+    ----------
+    E_i : ndarray
+        The reference point about which we compute the winding
+        number
+    E : ndarray
+        A point in phase space
+    
+    Returns
+    -------
+    float
+        The value of the integrand at E
+    """
+    x_diff, p_diff = E_i[0] - E[0], E_i[1] - E[1]
+
+    return 2*x_diff*p_diff/(m.pi*(x_diff**2 + p_diff**2)**2)
+
+def MC_mean_value(f, x_range, p_range, N, random_gen=None):
+    """
+    Computes the integral of f over a rectangular domain in phase space 
+    defined by (x_range, p_range) using the mean value Monte Carlo method 
+    with N samples.
+
+    Parameters
+    ----------
+    f : function
+        The integrand
+    x_range : ndarray
+        The x boundaries of the domain of integration
+    p_range : ndarray
+        The p boundaries of the domain of integration
+    N : int
+        The number of samples to be used
+    random_gen : str
+        Optional: the random number distribution to be used. If none 
+        specified, defaults to 2D uniform
+
+    Returns
+    -------
+    float
+        The value of the integral over the specified domain
+    """    
+
+
 # Testing
 
 if __name__ == '__main__':
     import pylab as pl 
+
+    # test hypercube
     x, p = np.array([0, 2]), np.array([0, 1])
     N = 10
     x_list, p_list = [], []
