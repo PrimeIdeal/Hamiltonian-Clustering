b62287e2cdacbe92ac31973f798a1c951d87d7d7
PrimeIdeal <49109135+PrimeIdeal@users.noreply.github.com>
2019-11-17 18:10:01 -0700

    added uniform and gaussian generators

--- integrals.py
+++ integrals.py
@@ -12,15 +12,74 @@ area enclosed by Y:
 If w(E_i, Y) = 1, then E_i is enclosed by Y. If w(E_i, Y) = 0, then E_i 
 is not enclosed by Y.
 
-Requirements: numpy, random, math
+Requirements: numpy, random, math, trajectory
 
 Date            Author              Description
 10/24/2019      Pierre Gauvreau     initial version - latin hypercube
+11/17/2019      Pierre Gauvreau     uniform and gaussian distributions
 """
 
 import numpy as np 
 import random as r 
 import math as m 
+from trajectory import H
+
+def gen_uniform(x_range, p_range, N):
+    """
+    Generates uniform random values of (x, p) in the rectangle specified
+    by (x_range, p_range) in phase space.
+
+    Parameters
+    ----------
+    x_range : ndarray
+        The x boundaries of the hypercube
+    p_range : ndarray
+        The p boundaries of the hypercube
+    N : int
+        The number of values to be generated
+
+    Yields
+    ------
+    ndarray
+        A point in phase space
+    """
+
+    x0, x1, p0, p1 = x_range[0], x_range[1], p_range[0], p_range[1]
+    x_dist, p_dist = x1-x0, p1-p0
+
+    for _i in range(N):
+        x, p = x0 + x_dist*r.random(), p0 + p_dist*r.random()
+        yield np.array([x, p])
+
+def gen_gaussian(x_range, p_range, N):
+    """
+    Generates gaussian random values of (x, p) in the rectangle specified
+    by (x_range, p_range) in phase space.
+
+    Parameters
+    ----------
+    x_range : ndarray
+        The x boundaries of the hypercube
+    p_range : ndarray
+        The p boundaries of the hypercube
+    N : int
+        The number of values to be generated
+
+    Yields
+    ------
+    ndarray
+        A point in phase space
+    """
+
+    x0, x1, p0, p1 = x_range[0], x_range[1], p_range[0], p_range[1]
+    x_dist, p_dist = x1-x0, p1-p0
+
+    mu_x, mu_p = x0 + x_dist/2, p0 + p_dist/2
+    sigma_x, sigma_p = 1/3, 1/3 # I will make this user-specifiable later
+    
+    for _i in range(N):
+        x, p = mu_x + mu_x*r.gauss(0, sigma_x)%1, mu_p + mu_p*r.gauss(0, sigma_p)%1
+        yield np.array([x, p])
 
 def gen_hypercube(x_range, p_range, N):
     """
@@ -41,6 +100,7 @@ def gen_hypercube(x_range, p_range, N):
     ndarray
         A point in phase space
     """
+
     x0, x1, p0, p1 = x_range[0], x_range[1], p_range[0], p_range[1]
     x_step, p_step = (x1-x0)/N, (p1-p0)/N
     x_samples = [x0 + k*x_step + x_step*r.random() for k in range(N)]
@@ -54,7 +114,7 @@ def gen_hypercube(x_range, p_range, N):
         p_idx = r.randint(0, len(p_samples) - 1)
         yield np.array([x_samples.pop(x_idx), p_samples.pop(p_idx)])
 
-def winding_integrand(E_i, E):
+def winding_integrand(E_i, E, H_r):
     """
     The transformed winding number function to be integrated over
     the domain enclosed by the trajectory.
@@ -65,17 +125,23 @@ def winding_integrand(E_i, E):
         number
     E : ndarray
         A point in phase space
+    H_r : float
+        Reference level of the trajectory
     
     Returns
     -------
     float
         The value of the integrand at E
     """
+
     x_diff, p_diff = E_i[0] - E[0], E_i[1] - E[1]
 
-    return 2*x_diff*p_diff/(m.pi*(x_diff**2 + p_diff**2)**2)
+    if H(E) < H_r:
+        return 0
+    else:
+        return 2*x_diff*p_diff/(m.pi*(x_diff**2 + p_diff**2)**2)
 
-def MC_mean_value(f, x_range, p_range, N, random_gen=None):
+def MC_mean_value(f, H_r, x_range, p_range, N, random_gen=None):
     """
     Computes the integral of f over a rectangular domain in phase space 
     defined by (x_range, p_range) using the mean value Monte Carlo method 
@@ -99,8 +165,14 @@ def MC_mean_value(f, x_range, p_range, N, random_gen=None):
     -------
     float
         The value of the integral over the specified domain
-    """    
-    pass
+    """
+
+    assert x_range[1] > x_range[0]
+    assert p_range[1] > p_range[0]
+
+    V = (x_range[1]-x_range[0])*(p_range[1]-p_range[0])
+
+    
 
 # Testing
 
